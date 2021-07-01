a956fe7c990bdd8ee15e06c1cf6961dd22f3e37d
csidevops <56381436+csidevops@users.noreply.github.com>
2021-07-01 14:05:47 +0530

    Update db.sql

--- dbFile/db.sql
+++ dbFile/db.sql
@@ -1,32 +1,20 @@
 CREATE DATABASE  IF NOT EXISTS `demoapp` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `demoapp`;
 
-DROP TABLE IF EXISTS `bookingdetails`;
+DROP TABLE IF EXISTS `user`;
 
-CREATE TABLE `bookingdetails` (
-  `bookingID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `bookingDate` datetime DEFAULT NULL,
-  `bookingTime` varchar(255) DEFAULT NULL,
-  `destination` varchar(255) DEFAULT NULL,
-  `expiryDate` datetime DEFAULT NULL,
-  `expiryTime` varchar(255) DEFAULT NULL,
-  `sourceLocation` varchar(255) DEFAULT NULL,
-  `carID` bigint(20) DEFAULT NULL,
-  `paymentID` bigint(20) DEFAULT NULL,
-  `userID` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`bookingID`),
-  UNIQUE KEY `bookingID` (`bookingID`),
-  KEY `FKAACCE7C91A39EF52` (`paymentID`),
-  KEY `FKAACCE7C9D3864E26` (`userID`),
-  KEY `FKAACCE7C95CCDCEE` (`carID`),
-  CONSTRAINT `FKAACCE7C91A39EF52` FOREIGN KEY (`paymentID`) REFERENCES `payment` (`paymentID`),
-  CONSTRAINT `FKAACCE7C95CCDCEE` FOREIGN KEY (`carID`) REFERENCES `car` (`RegistrationNo`),
-  CONSTRAINT `FKAACCE7C9D3864E26` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+CREATE TABLE `user` (
+  `userID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`userID`),
+  UNIQUE KEY `userID` (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-LOCK TABLES `bookingdetails` WRITE;
+LOCK TABLES `user` WRITE;
 
-INSERT INTO `bookingdetails` VALUES (3,'2016-04-27 00:00:00','12:00','LGA','2016-04-28 00:00:00','12:00','LGA',3,3,NULL);
+INSERT INTO `user` VALUES (1,'Rohit Agarwal','agarwal.ro@husky.neu.edu','qq');
 
 UNLOCK TABLES;
 
@@ -73,19 +61,32 @@ INSERT INTO `payment` VALUES (1,'Rohit Agarwal','6011859626355262','222','Rohit 
 
 UNLOCK TABLES;
 
-DROP TABLE IF EXISTS `user`;
 
-CREATE TABLE `user` (
-  `userID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`userID`),
-  UNIQUE KEY `userID` (`userID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `bookingdetails`;
 
-LOCK TABLES `user` WRITE;
+CREATE TABLE `bookingdetails` (
+  `bookingID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `bookingDate` datetime DEFAULT NULL,
+  `bookingTime` varchar(255) DEFAULT NULL,
+  `destination` varchar(255) DEFAULT NULL,
+  `expiryDate` datetime DEFAULT NULL,
+  `expiryTime` varchar(255) DEFAULT NULL,
+  `sourceLocation` varchar(255) DEFAULT NULL,
+  `carID` bigint(20) DEFAULT NULL,
+  `paymentID` bigint(20) DEFAULT NULL,
+  `userID` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`bookingID`),
+  UNIQUE KEY `bookingID` (`bookingID`),
+  KEY `FKAACCE7C91A39EF52` (`paymentID`),
+  KEY `FKAACCE7C9D3864E26` (`userID`),
+  KEY `FKAACCE7C95CCDCEE` (`carID`),
+  CONSTRAINT `FKAACCE7C91A39EF52` FOREIGN KEY (`paymentID`) REFERENCES `payment` (`paymentID`),
+  CONSTRAINT `FKAACCE7C95CCDCEE` FOREIGN KEY (`carID`) REFERENCES `car` (`RegistrationNo`),
+  CONSTRAINT `FKAACCE7C9D3864E26` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
-INSERT INTO `user` VALUES (1,'Rohit Agarwal','agarwal.ro@husky.neu.edu','qq');
+LOCK TABLES `bookingdetails` WRITE;
+
+INSERT INTO `bookingdetails` VALUES (3,'2016-04-27 00:00:00','12:00','LGA','2016-04-28 00:00:00','12:00','LGA',3,3,NULL);
 
 UNLOCK TABLES;
