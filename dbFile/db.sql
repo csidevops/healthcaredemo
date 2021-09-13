27b96e3e4caa7d6901c93248545b42f3372c0485
csidevops <56381436+csidevops@users.noreply.github.com>
2021-09-13 09:14:55 +0530

    Update db.sql

--- dbFile/db.sql
+++ dbFile/db.sql
@@ -1,4 +1,4 @@
 USE `demoapp`;
 DELETE FROM demoapp.user where Name = 'Demo User';
-INSERT INTO demoapp.user VALUES (1,'Demo User','demouser@healthcare.com','test');
+INSERT INTO demoapp.user VALUES (3,'Demo User','demouser@healthcare.com','test');
 select * from demoapp.user;
